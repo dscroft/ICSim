5272558b58ba4c2750117e12fb90e5376616505b
dscroft <dscroft@gmail.com>
2023-01-04 11:01:53 +0000

    Update setup_vcan.sh
    Easier to just solve the dependencies issue here.

--- setup_vcan.sh
+++ setup_vcan.sh
@@ -1,3 +1,7 @@
+# get the kernal module here
+sudo apt-get install -y linux-modules-extra-$(uname -r)
+
+# create vcan0 device
 sudo modprobe can
 sudo modprobe vcan
 sudo ip link add dev vcan0 type vcan
